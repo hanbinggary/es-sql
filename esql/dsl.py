d5d3dd83885c769b3a0e828527686de76ea000ba
yasinasama <614867000@qq.com>
2018-11-22 17:31:31 +0800

    update

--- esql/dsl.py
+++ esql/dsl.py
@@ -3,13 +3,29 @@
 
 class Aggregation:
     def __init__(self):
-        self.fcolumns = 
+        self.fcolumns = 1
         pass
+
+
+def make_dsl_where(dsl,conditions):
+    result = {
+        'bool':{
+            'must':[],
+            'should':[],
+            'must_not':[]
+        }
+    }
 
     
 
-def make_dsl_where(dsl,conditions):
-    result = self._model.bool_query
+def make_dsl_where2(dsl,conditions):
+    result = {
+        'bool':{
+            'must':[],
+            'should':[],
+            'must_not':[]
+        }
+    }
     if isinstance(conditions, list) and len(conditions) == 1:
         conditions = conditions[0]
     if 'OR' in conditions or 'AND' in conditions:
@@ -64,6 +80,61 @@ def make_dsl_limit(dsl,limit):
         dsl.setdefault('from',_from)
         dsl.setdefault('size',_size)
 
+
+def make_dsl_like(left,right):
+    '''
+    notice:
+        only support for [not analyzed] fields
+    * which matches any character sequence (including the empty one) 
+    ? which matches any single character
+
+    dsl
+    {
+        "wildcard": { "user" : "ki*y" }
+    }
+    '''
+    return {'wildcard': { left: right} }
+
+
+def make_dsl_in(left,right):
+    '''
+    notice:
+        only support for [not analyzed] fields
+
+    dsl
+    {
+        "terms": { "user" : ["one","two"] }
+    }
+    '''
+    return {'terms': {left: right} }
+
+def make_dsl_equal(left,right):
+    '''
+    query for exact values
+
+    dsl
+    {
+        "term": { "user" : "one" }
+    }
+    '''
+    return {'term': {left: right} }
+
+
+def make_dsl_range(left,right,compare):
+    '''
+    dsl
+    {
+        "range": { "age" : { "gte":10 } }
+    }
+    '''
+    compare_mapping = {
+        '>': 'gt',
+        '>=': 'gte',
+        '<': 'lt',
+        '<=': 'lte'
+    }
+    comp = compare_mapping[compare]
+    return {'range':{ left: {comp: right} } }
 
 
 
