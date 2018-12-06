345a4817b27cce10006daafcb5c3d3db98b641bf
yasinasama <614867000@qq.com>
2018-12-06 10:57:59 +0800

    add

--- esql/dsl.py
+++ esql/dsl.py
@@ -1,45 +1,185 @@
 # -*- coding: utf-8 -*-
 
+# from .common import split_list
+from itertools import groupby
+def split_list(source,wd):
+    return [list(g) for k, g in groupby(source, lambda x: x == wd) if not k]
 
 class Aggregation:
-    def __init__(self):
-        self.fcolumns = 1
-        pass
+    def __init__(self,columns,groups,havings):
+        self.columns = columns
+        self.groups = groups
+        self.havings = havings
+
+        self.fcolumns = [] # function columns
+        self.scolumns = [] # need to show 
+        self.aggs = {}
+
+        self._init_columns()
+
+    def _init_columns(self):
+        for column in self.columns:
+            name = column['name']
+            func = column['func']
+            if func:
+                self.fcolumns.append((name,func))
+            else:
+                self.scolumns.append(name)
+        if '*' in self.scolumns:
+            self.scolumns = []
 
 
-def make_dsl_where(dsl,conditions):
-    result = {
-        'bool':{
-            'must':[],
-            'should':[],
-            'must_not':[]
+    def _make_aggs_func(self,aggs):
+        for name,func in self.fcolumns:
+            if name == '*':
+                name = '_index'
+                metric_name = func
+            else:
+                metric_name = func+'_'+name
+            if func == 'count':
+                func = 'value_count'
+            metric = {func:{'field':name}}
+            aggs[metric_name] = metric
+
+
+    def make_dsl_group(self,index=0,aggs=None):
+        name = self.groups[index]
+        gl = len(self.groups)
+        subaggs = {}
+
+        if aggs is None:
+            aggs = self.aggs
+
+        aggs[name] = {
+            'aggs':subaggs,
+            # TODO size is not accurate
+            'terms': {'field': name,'size':'1000'}
         }
-    }
 
-    
-
-def make_dsl_where2(dsl,conditions):
-    result = {
-        'bool':{
-            'must':[],
-            'should':[],
-            'must_not':[]
-        }
-    }
-    if isinstance(conditions, list) and len(conditions) == 1:
-        conditions = conditions[0]
-    if 'OR' in conditions or 'AND' in conditions:
-        if 'OR' in conditions:
-            comb_k = 'OR'
+        # latest group
+        if index == gl-1:
+            if self.havings:
+                subaggs['having'] = {'bucket_selector':self._make_dsl_having(self.havings)}
+            # all funcion columns collected
+            self._make_aggs_func(subaggs)
         else:
-            comb_k = 'AND'
-        comb_v = Structure.COMB[comb_k]
-        subconds = self._split_list(conditions,comb_k)
+            self.make_dsl_group(index+1,subaggs)
+
+
+    def _make_dsl_having(self,havings):
+        selector = {'buckets_path':{},'script':''}
+        for having in havings:
+            if isinstance(having,dict):
+                name, func, right, compare = having['left']['name'], \
+                                             having['left']['func'], \
+                                             having['right'], \
+                                             having['compare']
+
+                path_value = func+'_'+name
+                path_name = 'val_'+path_value
+                selector['buckets_path'][path_name] = path_value
+
+                if (name,func) not in self.fcolumns:
+                    self.fcolumns.append((name,func))
+                if compare == '=':
+                    compare = '=='
+                if compare == '<>':
+                    compare = '!='
+                selector['script']+=' (%s %s %s) '%(path_name,compare,right)
+            elif isinstance(having,str):
+                if having == 'AND':
+                    comb = '&&'
+                else:
+                    comb = '||'
+                selector['script']+=' %s '%comb
+            else:# list
+                subhaving = self._make_dsl_having(having)
+                selector['buckets_path'].update(subhaving['buckets_path'])
+                selector['script']+=' (%s) '%subhaving['script']
+        return selector
+
+
+def make_dsl_where(conditions):
+    '''
+    dsl: select * from test where tt = 123 and tt = 11;
+    {
+        "bool": 
+            {
+                "must": 
+                    [
+                        {
+                            "bool": 
+                                {
+                                    "must": 
+                                        [
+                                            {
+                                                "term": {"tt": 123}
+                                            }
+                                        ], 
+                                    "should": [], 
+                                    "must_not": []
+                                }
+                        }, 
+                        {
+                            "bool": 
+                                {
+                                    "must": 
+                                        [
+                                            {
+                                                "term": {"tt": 11}
+                                            }
+                                        ], 
+                                    "should": [], 
+                                    "must_not": []
+                                }
+                        }
+                    ], 
+                "should": [], 
+                "must_not": []
+            }
+    }
+    '''
+    result = {
+        'bool':{
+            'must':[],
+            'should':[],
+            'must_not':[]
+        }
+    }
+    # filter 
+    while isinstance(conditions, list) and len(conditions) == 1:
+        conditions = conditions[0]
+
+    # `AND` has high priority,so we should split `OR` first
+    l = ['OR' in conditions,'AND' in conditions]
+    if any(l):
+        if l[0]:
+            _conn,_bool = 'OR','should'
+        else:
+            _conn,_bool = 'AND','must'
+        subconds = split_list(conditions,_conn)
         for subcond in subconds:
-            result['bool'][comb_v].append(self.struct_where(subcond))
+            result['bool'][_bool].append(make_dsl_where(subcond))
     else:
-        subquery, comb = self._subquery(conditions, 'must')
-        result['bool'][comb].append(subquery)
+        left = conditions['left']['name']
+        right = conditions['right']
+        compare = conditions['compare']
+
+        _bool = 'must'
+
+        if compare == 'LIKE':
+            comp_dsl = make_dsl_like(left,right)
+        elif compare == 'IN':
+            comp_dsl = make_dsl_in(left,right)
+        elif compare == '=':
+            comp_dsl = make_dsl_equal(left,right)
+        elif compare in ('<>','!='):
+            _bool = 'must_not'
+            comp_dsl = make_dsl_not_equal(left,right)
+        else:
+            comp_dsl = make_dsl_range(left,right,compare)
+
+        result['bool'][_bool].append(comp_dsl)
     return result
 
 
@@ -136,5 +276,23 @@ def make_dsl_range(left,right,compare):
     comp = compare_mapping[compare]
     return {'range':{ left: {comp: right} } }
 
+def make_dsl_not_equal(left,right):
+    '''
+    dsl:
 
+    must_not
+    {
+        "term": { "user" : "one" }
+    }
+    '''
+    return {'term': {left: right} }
+
+
+if __name__=='__main__':
+    c =  [{'name': '*', 'func': ''}]
+    g= ['a', 'b', 'c']
+    h = [[[{'left': {'name': 'a', 'func': 'count'}, 'right': 10, 'compare': '>'}, 'AND', {'left': {'name': 'b', 'func': 'avg'}, 'right': 5, 'compare': '<'}]]]
+    a=Aggregation(c,g,h)
+    a.make_dsl_group()
+    print(a.aggs)
 
